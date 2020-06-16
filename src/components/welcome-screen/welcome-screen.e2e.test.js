eff249e11c903feb66ef7f5cf128b0c13db8ab79
o-strekalova <strekalova.olg@yandex.ru>
2020-06-17 00:44:02 +0300

    Добавляет тесты для компонентов

---
+++ src/components/welcome-screen/welcome-screen.e2e.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Enzyme, {shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import WelcomeScreen from "./welcome-screen";
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+
+it(`Press welcome button`, () => {
+  const onWelcomeButtonClick = jest.fn();
+
+  const welcomeScreen = shallow(
+      <WelcomeScreen
+        errorsCount={3}
+        onWelcomeButtonClick={onWelcomeButtonClick}
+      />
+  );
+
+  const welcomeButton = welcomeScreen.find(`button.welcome__button`);
+  welcomeButton.simulate(`click`);
+
+  expect(onWelcomeButtonClick.mock.calls.length).toBe(1);
+});
